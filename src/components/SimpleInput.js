34322e9a66161c21ce9f1dd1a56db55cc78898a4
Prateek Varma <prtk.varma@gmail.com>
2022-09-15 14:32:00 -0600

    used state vs useRef() hook to extract data from input field

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+
+  const nameInputRef = useRef();
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log("Using ref: ", enteredValue);
+    setEnteredName(""); //clear input field
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={enteredName} />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
