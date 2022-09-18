3c5ab5680a945777dae1fc3667c5366974c679a1
Prateek Varma <prtk.varma@gmail.com>
2022-09-18 17:21:06 -0600

    Added another state, that managed the entire form's validity as a whole, and sets button as disabled when invalid

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false); //to check if the name field is yet entered or not.
+  const [formIsValid, setFormIsValid] = useState(false); //for the entire form, not just an element
 
   const enteredNameIsValid = enteredName.trim() !== ""; //replaced an entire state with a simple boolean expression.
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched; //if user has initially typed into the form, and the field is valid.
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      //if had more fields we could if(enteredNameIsValid && ageIsValid)
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false); //if one of the fields are invalid, entire form is invaid
+    }``
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -50,7 +60,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
